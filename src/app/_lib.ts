f27ee0c6ede96148c8a67bb9c083e9aa4d90356c
KATT <alexander@n1s.se>
2023-05-12 17:53:31 +0200

    clean

--- src/app/_lib.ts
+++
@@ -1,8 +0,0 @@
-import { AnyProcedure, inferProcedureOutput } from "@trpc/server";
-
-export function asAction<T extends AnyProcedure>(
-  proc: T,
-): Promise<inferProcedureOutput<T>> {
-  // ... call procedure
-  throw new Error();
-}
